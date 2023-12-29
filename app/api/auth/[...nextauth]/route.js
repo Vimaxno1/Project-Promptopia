dac6e0c2b4ea00a254c949ef3edc07a3e8d39da3
Vimaxno1 <104040348+Vimaxno1@users.noreply.github.com>
2023-12-29 10:39:07 +0100

    Changes made

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,41 +9,39 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    })
   ],
   callbacks: {
     async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
-
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
+
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
-            usename: profile.name.replace(' ', '').toLowerCase(),
+            username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture,
           });
         }
 
-        return true;
+        return true
       } catch (error) {
-        console.log(error);
-
-        return false;
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
     },
-  },
-});
+  }
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
